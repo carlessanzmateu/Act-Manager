45f73c3ad44d21f08137ffef87d5bc4c6171cf98
carles <carles.sanz.contractor@dev.bbva.com>
2018-11-08 09:24:05 +0100

    Integrate rehearsals from service on rehearsals page

--- src/app/rehearsals/rehearsals.page.ts
+++ src/app/rehearsals/rehearsals.page.ts
@@ -1,36 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { RehearsalsService } from '../services/rehearsals/rehearsals.service';
-
+import { Rehearsal } from '../../models/rehearsal.model';
 @Component({
   selector: 'app-rehearsals',
   templateUrl: 'rehearsals.page.html',
   styleUrls: ['rehearsals.page.scss']
 })
 export class RehearsalsPage implements OnInit {
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor(public rehearsalsService: RehearsalsService) {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
+  
+  public items: Array<Rehearsal> = [];
+
+  constructor(public rehearsalsService: RehearsalsService) { }
 
   ngOnInit() {
     console.log(this.rehearsalsService.getRehearsals());
+    this.items = this.rehearsalsService.getRehearsals();
   }
 }
